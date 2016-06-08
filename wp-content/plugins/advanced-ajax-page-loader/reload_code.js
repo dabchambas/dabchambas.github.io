efac7e150b52ac47237af9773b8a4dfc8e3550fc
blakroku <blakroku@gmail.com>
2016-06-08 14:19:17 +0000

    Initial commit for Demo Design

---
+++ wp-content/plugins/advanced-ajax-page-loader/reload_code.js
@@ -0,0 +1,17 @@
+function AAPL_reload_code() {
+//This file is generated from the admin panel - dont edit here! 
+init_map();
+body();
+}
+
+function AAPL_click_code(thiss) {
+//This file is generated from the admin panel - dont edit here! 
+// highlight the current menu item
+jQuery("li").removeClass('current-menu-item active');
+jQuery(thiss).parent("li").addClass('current-menu-item active');
+}
+
+function AAPL_data_code(dataa) {
+//This file is generated from the admin panel - dont edit here! 
+
+}
